10dcefe2a5741e81166e844404aa1948774e4efe
Simon Uggla <simon.uggla@gmail.com>
2022-12-01 07:55:59 +0100

    day 01

---
+++ src/bin/01.rs
@@ -0,0 +1,33 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    input.split("\n\n").map(|elf| elf.lines().map(|n| n.parse::<u32>().unwrap()).sum()).max()
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut x = input.split("\n\n").map(|elf| elf.lines().map(|n| n.parse::<u32>().unwrap()).sum()).collect::<Vec<u32>>();
+    x.sort();
+    x.reverse();
+    Some(x.iter().take(3).sum::<u32>())
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 1);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_one(&input).unwrap(), 24000);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_two(&input), None);
+    }
+}
