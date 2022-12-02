a5a6d9e73ee954a6f9cab1fba456ee29b44962d7
Simon Uggla <simon.uggla@gmail.com>
2022-12-02 07:24:32 +0100

    Added examples

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -5,8 +5,7 @@ pub fn part_one(input: &str) -> Option<u32> {
 pub fn part_two(input: &str) -> Option<u32> {
     let mut x = input.split("\n\n").map(|elf| elf.lines().map(|n| n.parse::<u32>().unwrap()).sum()).collect::<Vec<u32>>();
     x.sort();
-    x.reverse();
-    Some(x.iter().take(3).sum::<u32>())
+    Some(x.iter().rev().take(3).sum::<u32>())
 }
 
 fn main() {
@@ -22,12 +21,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_one(&input).unwrap(), 24000);
+        assert_eq!(part_one(&input), Some(24000));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(45000));
     }
 }
