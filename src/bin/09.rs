9681ad78b191b323857288df4861663300727681
Simon Uggla <simon.uggla@gmail.com>
2022-12-09 20:02:36 +0000

    One liners-ish

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -12,7 +12,26 @@ fn parse(input: &str) -> Vec<char> {
     .concat()
 }
 
-fn new_pos((head_x, head_y): Pos, (tail_x, tail_y): Pos) -> Pos {
+fn solve<A, B, C: PartialEq + std::cmp::Ord, F>(
+  instructions: Vec<char>,
+  init: (A, B, Vec<C>),
+  f: F,
+) -> usize
+where
+  F: FnMut((A, B, Vec<C>), &char) -> (A, B, Vec<C>),
+{
+  instructions
+    .iter()
+    .fold(init, f)
+    .2
+    .iter()
+    .sorted()
+    .dedup()
+    .collect_vec()
+    .len()
+}
+
+fn move_tail((head_x, head_y): Pos, (tail_x, tail_y): Pos) -> Pos {
   let (diff_x, diff_y) = (head_x - tail_x, head_y - tail_y);
   if i32::abs(diff_x) >= 2 && i32::abs(diff_y) >= 2 {
     return (
@@ -27,7 +46,7 @@ fn new_pos((head_x, head_y): Pos, (tail_x, tail_y): Pos) -> Pos {
   (tail_x, tail_y)
 }
 
-fn move_pos((pos_x, pos_y): Pos, dir: char) -> Pos {
+fn move_head((pos_x, pos_y): Pos, dir: char) -> Pos {
   match dir {
     'U' => (pos_x, pos_y + 1),
     'D' => (pos_x, pos_y - 1),
@@ -38,43 +57,33 @@ fn move_pos((pos_x, pos_y): Pos, dir: char) -> Pos {
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
-  let mut x = parse(input)
-    .iter()
-    .fold(
-      ((0, 0), (0, 0), Vec::new()),
-      |(head, tail, mut prev), dir| {
-        let head_pos = move_pos(head, *dir);
-        let tail_pos = new_pos(head_pos, tail);
-        prev.push(tail);
-        (head_pos, tail_pos, prev)
-      },
-    )
-    .2;
-  x.sort();
-  x.dedup();
-  Some(x.len())
+  Some(solve(
+    parse(input),
+    ((0, 0), (0, 0), Vec::new()),
+    |(head, tail, mut prev), dir| {
+      let head_pos = move_head(head, *dir);
+      let tail_pos = move_tail(head_pos, tail);
+      prev.push(tail);
+      (head_pos, tail_pos, prev)
+    },
+  ))
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-  let mut x = parse(input)
-    .iter()
-    .fold(
-      ((0, 0), vec![(0, 0); 9], Vec::new()),
-      |(head, tail, mut prev), dir| {
-        let head_pos = move_pos(head, *dir);
-        let mut new_pieces: Vec<Pos> = vec![];
-        for piece in tail {
-          new_pieces.push(new_pos(*new_pieces.last().unwrap_or(&head_pos), piece));
-        }
-        let last = new_pieces.last().unwrap();
-        prev.push(*last);
-        (head_pos, new_pieces, prev)
-      },
-    )
-    .2;
-  x.sort();
-  x.dedup();
-  Some(x.len())
+  Some(solve(
+    parse(input),
+    ((0, 0), vec![(0, 0); 9], Vec::new()),
+    |(head, tail, mut prev), dir| {
+      let head_pos = move_head(head, *dir);
+      let mut new_pieces: Vec<Pos> = vec![];
+      for piece in tail {
+        new_pieces.push(move_tail(*new_pieces.last().unwrap_or(&head_pos), piece));
+      }
+      let last = new_pieces.last().unwrap();
+      prev.push(*last);
+      (head_pos, new_pieces, prev)
+    },
+  ))
 }
 
 fn main() {
