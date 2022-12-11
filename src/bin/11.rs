68744f8c43365db51ecd190cadf86cbfaa015025
Simon Uggla <simon.uggla@gmail.com>
2022-12-11 12:31:58 +0000

    Move implementation to own function

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -56,13 +56,13 @@ impl Monkey {
   }
 }
 
-pub fn part_one(input: &str) -> Option<Size> {
+fn solve<F>(input: &str, loop_count: u32, f: F) -> Size where F: Fn(&mut Size, Size) {
   let mut mod_to_rule_them_all: Size = 1;
   let mut monkeys = input.split("\n\n").map(Monkey::from_str).collect_vec();
   for monke in &monkeys {
     mod_to_rule_them_all *= monke.test;
   }
-  for _ in 0..20 {
+  for _ in 0..loop_count {
     for i in 0..monkeys.len() {
       let (first, rest) = monkeys.split_at_mut(i);
       let (monke, rest) = rest.split_first_mut().unwrap();
@@ -74,8 +74,8 @@ pub fn part_one(input: &str) -> Option<Size> {
           Op::Square => item * item,
         };
         
-        item %= mod_to_rule_them_all;
-        item /= 3;
+
+        f(&mut item, mod_to_rule_them_all);
 
         let test = usize::from(item % monke.test == 0);
         let throw_to = *monke.throw_to.get(test).unwrap() as usize;
@@ -91,44 +91,20 @@ pub fn part_one(input: &str) -> Option<Size> {
     }
   }
   let x = monkeys.iter().map(|m| m.items_handeled).sorted().rev().take(2).collect_vec();
-  Some(x.first().unwrap() * x.get(1).unwrap())
+  x.first().unwrap() * x.get(1).unwrap()
+}
+
+pub fn part_one(input: &str) -> Option<Size> {
+  Some(solve(input, 20, |item, mod_num| {
+    *item %= mod_num;
+    *item /= 3;
+  }))
 }
 
 pub fn part_two(input: &str) -> Option<Size> {
-  let mut mod_to_rule_them_all: Size = 1;
-  let mut monkeys = input.split("\n\n").map(Monkey::from_str).collect_vec();
-  for monke in &monkeys {
-    mod_to_rule_them_all *= monke.test;
-  }
-  for _ in 0..10000 {
-    for i in 0..monkeys.len() {
-      let (first, rest) = monkeys.split_at_mut(i);
-      let (monke, rest) = rest.split_first_mut().unwrap();
-      while !monke.items.is_empty() {
-        let item = monke.items.pop_front().unwrap();
-        let mut item = match monke.op {
-          Op::Multiply(v) => item * v,
-          Op::Add(v) => item + v,
-          Op::Square => item * item,
-        };
-        
-        item %= mod_to_rule_them_all;
-
-        let test = usize::from(item % monke.test == 0);
-        let throw_to = *monke.throw_to.get(test).unwrap() as usize;
-
-        if throw_to < i {
-          first.get_mut(throw_to).unwrap().items.push_back(item);
-        } else {
-          rest.get_mut(throw_to - i - 1).unwrap().items.push_back(item);
-        }
-
-        monke.items_handeled += 1;
-      }
-    }
-  }
-  let x = monkeys.iter().map(|m| m.items_handeled).sorted().rev().take(2).collect_vec();
-  Some(x.first().unwrap() * x.get(1).unwrap())
+  Some(solve(input, 10000, |item, mod_num| {
+    *item %= mod_num;
+  }))
 }
 
 fn main() {
