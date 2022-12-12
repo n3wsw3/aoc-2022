7dcbf925e614b879d5ad015f39cd9f4c59f12ef2
Simon Uggla <simon.uggla@gmail.com>
2022-12-12 11:42:43 +0000

    Fix indentation in template

--- src/bin/scaffold.rs
+++ src/bin/scaffold.rs
@@ -9,34 +9,34 @@ use std::{
 };
 
 const MODULE_TEMPLATE: &str = r###"pub fn part_one(input: &str) -> Option<u32> {
-    None
+  None
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+  None
 }
 
 fn main() {
-    let input = &advent_of_code::read_file("inputs", DAY);
-    advent_of_code::solve!(1, part_one, input);
-    advent_of_code::solve!(2, part_two, input);
+  let input = &advent_of_code::read_file("inputs", DAY);
+  advent_of_code::solve!(1, part_one, input);
+  advent_of_code::solve!(2, part_two, input);
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn test_part_one() {
-        let input = advent_of_code::read_file("examples", DAY);
-        assert_eq!(part_one(&input), None);
-    }
+  #[test]
+  fn test_part_one() {
+    let input = advent_of_code::read_file("examples", DAY);
+    assert_eq!(part_one(&input), None);
+  }
 
-    #[test]
-    fn test_part_two() {
-        let input = advent_of_code::read_file("examples", DAY);
-        assert_eq!(part_two(&input), None);
-    }
+  #[test]
+  fn test_part_two() {
+    let input = advent_of_code::read_file("examples", DAY);
+    assert_eq!(part_two(&input), None);
+  }
 }
 "###;
 
